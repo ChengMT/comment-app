cdd59b56344c053a99cbdd96f359bf36357f86a5
chengmt <cn_chengmt@sina.com>
2018-07-11 22:33:52 +0800

    整体架构

---
+++ src/CommentInput.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class CommentInput extends Component{
+    render(){
+        return(
+            <div>
+                a
+            </div>
+        );
+    }
+
+}
+
+export default CommentInput;

2a1cf89a009380720010b4b28609ffe66ec8b5bd
chengmt <cn_chengmt@sina.com>
2018-07-18 21:08:08 +0800

    功能二

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -3,10 +3,19 @@ import CommentInput from './CommentInput';
 import CommentList from './CommentList';
 
 class CommentApp extends Component{
+    constructor(){
+        super();
+        this.handleSubmitComment = this.handleSubmitComment.bind(this);
+    }
+
+    handleSubmitComment (comment) {
+        console.log(comment)
+      }
+
     render(){
         return(
             <div className='wrapper'>
-                <CommentInput />
+                <CommentInput onSubmit={this.handleSubmitComment}/>
                 <CommentList />
             </div>
         );

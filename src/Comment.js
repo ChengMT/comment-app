7903695999e2c4f0a35d9fad073819d8e38dc667
chengmt <cn_chengmt@sina.com>
2018-07-22 11:15:09 +0800

    完成

--- src/Comment.js
+++ src/Comment.js
@@ -3,8 +3,11 @@ import React , {Component} from 'react';
 class Comment extends Component{
     render(){
         return(
-            <div>
-                c
+            <div className='comment'>
+                <div className='comment-user'>
+                    <span> {this.props.comment.username}：</span>
+                </div>
+                <p>{this.props.comment.content}</p>
             </div>
         );
     }
--- src/CommentApp.js
+++ src/CommentApp.js
@@ -5,18 +5,24 @@ import CommentList from './CommentList';
 class CommentApp extends Component{
     constructor(){
         super();
+        this.state = {
+            comments:[]
+        };
         this.handleSubmitComment = this.handleSubmitComment.bind(this);
     }
 
     handleSubmitComment (comment) {
-        console.log(comment)
+        this.state.comments.push(comment);
+        this.setState({
+            comments:this.state.comments
+        })
       }
 
     render(){
         return(
             <div className='wrapper'>
                 <CommentInput onSubmit={this.handleSubmitComment}/>
-                <CommentList />
+                <CommentList comments={this.state.comments}/>
             </div>
         );
     }
